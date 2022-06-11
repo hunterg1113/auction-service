fe9af209d3677ca4890c33a1ba99d003b0ba4c89
Hunter <huntergreer@gmail.com>
2022-06-11 10:20:13 -0500

    more place bid validation

--- src/handlers/placeBid.js
+++ src/handlers/placeBid.js
@@ -21,6 +21,10 @@ async function placeBid(event, context) {
 
   const auction = await getAuctionById(id)
   
+  if(auction.status !== 'OPEN') {
+    throw new createError.Forbidden(`You cannot place bids on closed auctions.`)
+  }
+
   if(amount <= auction.highestBid.amount){
     throw new createError.Forbidden(`Your bid must be higher than ${auction.highestBid.amount}`)
   }
