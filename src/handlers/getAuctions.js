0458754d6d4547ba051842dc94cccab0dfbadea6
Hunter <huntergreer@gmail.com>
2022-06-11 10:40:56 -0500

    query auctions by status, add validation schema

--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -1,17 +1,30 @@
 import AWS from 'aws-sdk'
+import validator from '@middy/validator'
 import createError from 'http-errors'
 import commonMiddleware from '../lib/commonMiddleware'
+import getAuctionsSchema from '../lib/schemas/getAuctionsSchema'
 
 const dynamodb = new AWS.DynamoDB.DocumentClient()
 
 async function getAuctions(event, context) {
+  const { status } = event.queryStringParameters
   let auctions;
 
-  try {
-    const result = await dynamodb.scan({
-      TableName: process.env.AUCTIONS_TABLE_NAME
-    }).promise()
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    IndexName: 'statusAndEndDate',
+    KeyConditionExpression: '#status = :status',
+    ExpressionAttributeValues: {
+      ':status': status,
+    },
+    ExpressionAttributeNames: {
+      '#status': 'status'
+    }
+  }
 
+  
+  try {
+    const result = await dynamodb.query(params).promise()
     auctions = result.Items
   } catch(error) {
     console.error(error)
@@ -25,3 +38,4 @@ async function getAuctions(event, context) {
 }
 
 export const handler = commonMiddleware(getAuctions)
+  .use(validator({ inputSchema: getAuctionsSchema, useDefaults: true }))
