7d53b6c110bcd491deeea38aabca43578715ce51
Hunter <huntergreer@gmail.com>
2022-06-10 22:44:37 -0500

    CRUD: place bid

---
+++ src/handlers/placeBid.js
@@ -0,0 +1,41 @@
+import AWS from 'aws-sdk'
+import createError from 'http-errors'
+import commonMiddleware from '../lib/commonMiddleware'
+
+const dynamodb = new AWS.DynamoDB.DocumentClient()
+
+async function placeBid(event, context) {
+  const { id } = event.pathParameters
+  const { amount } = event.body
+  
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    Key: { id },
+    UpdateExpression: 'set highestBid.amount = :amount',
+    ExpressionAttributeValues: {
+      ':amount': amount
+    },
+    ReturnValues: 'ALL_NEW'
+  }
+
+  let updatedAuction;
+  
+  try {
+    const result = await dynamodb.update(params).promise()
+    updatedAuction = result.Attributes
+  } catch(error) {
+    console.error(error)
+    throw new createError.InternalServerError(error)
+  }
+
+  if(!updatedAuction) {
+    throw new createError.NotFound(`Auction with ID "${id}" not found!`)
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(updatedAuction),
+  }
+}
+
+export const handler = commonMiddleware(placeBid)
