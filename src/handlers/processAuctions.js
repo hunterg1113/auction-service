05f6a739eb848d03c92c41d6aee8fbd9d4bf5e5a
Hunter <huntergreer@gmail.com>
2022-06-11 10:13:54 -0500

    update ended auction status

--- src/handlers/processAuctions.js
+++ src/handlers/processAuctions.js
@@ -1,15 +1,17 @@
 import createError from 'http-errors'
+import { closeAuction } from '../lib/closeAuction'
 import { getEndedAuctions } from '../lib/getEndedAuctions'
 
 async function processAuctions(event, context) {
   try {
     const auctionsToClose = await getEndedAuctions()
-    console.log(auctionsToClose)
+    const closePromises = auctionsToClose.map(auction => closeAuction(auction))
+    await Promise.all(closePromises)
+    return { closed: closePromises.length }
   } catch(error) {
     console.error(error)
     throw new createError.InternalServerError(error)
   }
-  console.log('processAuctions')
 }
 
 export const handler = processAuctions
