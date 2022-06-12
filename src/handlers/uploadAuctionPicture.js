0ff1977adc16d730a70628363200356083301098
Hunter <huntergreer@gmail.com>
2022-06-12 11:28:01 -0500

    add s3 bucket for image upload

---
+++ src/handlers/uploadAuctionPicture.js
@@ -0,0 +1,10 @@
+import AWS from 'aws-sdk'
+
+export async function uploadAuctionPicture(event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({})
+  }
+}
+
+export const handler = uploadAuctionPicture
