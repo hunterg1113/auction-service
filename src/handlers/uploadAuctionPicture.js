bc51f3d61ce13791d6e1b196ba20e26491415b4d
Hunter <huntergreer@gmail.com>
2022-06-12 13:18:15 -0500

    add pictureUrl prop to dynamoDB on image upload

--- src/handlers/uploadAuctionPicture.js
+++ src/handlers/uploadAuctionPicture.js
@@ -1,8 +1,10 @@
+import AWS from 'aws-sdk'
 import { getAuctionById } from './getAuction'
 import middy from '@middy/core'
 import httpErrorHandler from '@middy/http-error-handler'
 import createError from 'http-errors'
 import { uploadPictureToS3 } from '../lib/uploadPictureToS3'
+import { setAuctionPictureUrl } from '../lib/setAuctionPictureUrl'
 
 export async function uploadAuctionPicture(event, context) {
   const { id } = event.pathParameters
@@ -10,9 +12,10 @@ export async function uploadAuctionPicture(event, context) {
   const base64 = event.body.replace(/^data:image\/\w+;base64,/, '')
   const buffer = Buffer.from(base64, 'base64')
 
+  let updatedAuction
   try {
-    const uploadToS3Result = await uploadPictureToS3(auction.id + '.jpg', buffer)
-    console.log(uploadToS3Result)
+    const pictureUrl = await uploadPictureToS3(auction.id + '.jpg', buffer)
+    updatedAuction = await setAuctionPictureUrl(auction.id, pictureUrl)
   } catch(error) {
     console.error(error)
     throw new createError.InternalServerError(error)
@@ -20,9 +23,9 @@ export async function uploadAuctionPicture(event, context) {
   
   return {
     statusCode: 200,
-    body: JSON.stringify({})
+    body: JSON.stringify(updatedAuction)
   }
 }
 
 export const handler = middy(uploadAuctionPicture)
-  .use(httpErrorHandler)
+  .use(httpErrorHandler())
