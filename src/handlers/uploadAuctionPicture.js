c841cc2522b57ca1abd776a3845e475cc2e9373f
Hunter <huntergreer@gmail.com>
2022-06-12 12:58:22 -0500

    upload picture to s3 handler

--- src/handlers/uploadAuctionPicture.js
+++ src/handlers/uploadAuctionPicture.js
@@ -1,10 +1,28 @@
-import AWS from 'aws-sdk'
+import { getAuctionById } from './getAuction'
+import middy from '@middy/core'
+import httpErrorHandler from '@middy/http-error-handler'
+import createError from 'http-errors'
+import { uploadPictureToS3 } from '../lib/uploadPictureToS3'
 
 export async function uploadAuctionPicture(event, context) {
+  const { id } = event.pathParameters
+  const auction = await getAuctionById(id)
+  const base64 = event.body.replace(/^data:image\/\w+;base64,/, '')
+  const buffer = Buffer.from(base64, 'base64')
+
+  try {
+    const uploadToS3Result = await uploadPictureToS3(auction.id + '.jpg', buffer)
+    console.log(uploadToS3Result)
+  } catch(error) {
+    console.error(error)
+    throw new createError.InternalServerError(error)
+  }
+  
   return {
     statusCode: 200,
     body: JSON.stringify({})
   }
 }
 
-export const handler = uploadAuctionPicture
+export const handler = middy(uploadAuctionPicture)
+  .use(httpErrorHandler)
